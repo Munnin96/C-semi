75893ed38449eafa5b9f625bab3c3210dfefd9cf
Kirill <munnin95@gmail.com>
2023-01-13 22:13:39 +0300

    Задача 1

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -2,30 +2,17 @@
 Console.WriteLine("Введите номер");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number == 1)
-{
     Console.WriteLine("Соответствующий номеру день - Понедельник");
-}
-if (number == 2)
-{
+else if (number == 2)
     Console.WriteLine("Соответствующий номеру день - Вторник");
-}
-if (number == 3)
-{
+else if (number == 3)
     Console.WriteLine("Соответствующий номеру день - Среда");
-}
-if (number == 4)
-{
+else if (number == 4)
     Console.WriteLine("Соответствующий номеру день - Четверг");
-}
-if (number == 5)
-{
+else if (number == 5)
     Console.WriteLine("Соответствующий номеру день - Пятница");
-}
-if (number == 6)
-{
+else if (number == 6)
     Console.WriteLine("Соответствующий номеру день - Суббота");
-}
-if (number == 7)
-{
+else if (number == 7)
     Console.WriteLine("Соответствующий номеру день - Воскресенье");
-}
+else Console.WriteLine("Введено некорректное значение");
