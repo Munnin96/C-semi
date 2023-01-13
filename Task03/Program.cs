362c496788a0f23de18a02973819b96a48b5fd86
Kirill <munnin95@gmail.com>
2023-01-13 22:01:33 +0300

    Задача 1

---
+++ Task03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число a");
+int numbera = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int numberb = Convert.ToInt32(Console.ReadLine());
+if(numbera * numbera == numberb )
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
