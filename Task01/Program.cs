b3f6b1b894688c59d39afd4d487c2e4653960448
Kirill <munnin95@gmail.com>
2023-01-13 21:40:50 +0300

    Сделали задание 3

---
+++ Task01/Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("Введите номер");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine("Соответствующий номеру день - Понедельник");
+}
+if (number == 2)
+{
+    Console.WriteLine("Соответствующий номеру день - Вторник");
+}
+if (number == 3)
+{
+    Console.WriteLine("Соответствующий номеру день - Среда");
+}
+if (number == 4)
+{
+    Console.WriteLine("Соответствующий номеру день - Четверг");
+}
+if (number == 5)
+{
+    Console.WriteLine("Соответствующий номеру день - Пятница");
+}
+if (number == 6)
+{
+    Console.WriteLine("Соответствующий номеру день - Суббота");
+}
+if (number == 7)
+{
+    Console.WriteLine("Соответствующий номеру день - Воскресенье");
+}
